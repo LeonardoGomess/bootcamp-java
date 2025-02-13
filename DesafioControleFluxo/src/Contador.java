45b1429669c3d9b391661ab14fde727d4a0d073c
Leonardogomess <leoarantesgomes@gmail.com>
2025-02-13 14:43:21 -0300

    resolucao do desafio

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = scanner.nextInt();
+
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = scanner.nextInt();
+
+        try {
+            //chamando o método contendo a lógica de contagem
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm > parametroDois) throw new ParametrosInvalidosException();
+
+
+        int contagem = parametroDois - parametroUm;
+        //realizar o for para imprimir os números com base na variável contagem
+
+        for(int i=1; i<=contagem;i++){
+            System.out.println("Imprimindo o número " + i);
+        }
     }
 }
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,2 +1,4 @@
-public class ParametrosInvalidosException {
+public class ParametrosInvalidosException extends  Exception{
+
+
 }
